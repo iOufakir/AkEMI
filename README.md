d093fb330eabb54d7f3dbeb68bb79d5ca1864c10
Ilyas Oufakir <ouf.ilyas@gmail.com>
2024-05-09 21:18:01 -0400

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # AkEMI
+
+## Setup Instructions
+
+1. **Navigate to the Project Directory:** 
+    ```bash
+    cd nodejs-app
+    ```
+
+2. **Install Dependencies:** 
+    ```bash
+    npm install
+    ```
+
+3. **Start the Server:** 
+    ```bash
+    npm start
+    ```
+
+4. **Access the App:** 
+    - **Client Side:** [http://localhost:3000](http://localhost:3000)
+    - **Example of Web Service Backend Endpoint:** [http://localhost:3000/api/data](http://localhost:3000/api/data)
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+
+/**************** Backend endpoints ****************/
+app.get('/api/data', (req, res) => {
+    
+    console.info("THIS IS GET REQUEST");
+
+    res.json({ message: 'Backend endpoint reached!' });
+});
+
+app.post('/api/data', (req, res) => {
+    const { temperature, humidity } = req.body;
+
+    console.log('Received Temperature:', temperature);
+    console.log('Received Humidity:', humidity);
+
+    res.json({ message: 'Data has been saved successfully!' });
+});
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.info(`Server is running on port ${PORT}`);
+});
