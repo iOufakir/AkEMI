646bc04315a57d8820aaa40cb23c97554a0d3010
Ilyas Oufakir <ouf.ilyas@gmail.com>
2024-05-15 14:38:12 -0400

    Add db port confi

--- api/index.js
+++ api/index.js
@@ -18,7 +18,8 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT | 3306
 });
 
 // Connect to MySQL
